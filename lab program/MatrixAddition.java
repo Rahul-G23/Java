662b1ae3d1c3ed25edd4025110fd0756c9db7de4
Rahul G <rahulg23102006@gmail.com>
2025-11-17 14:23:43 +0530

    completing the lab program 1

--- lab program/MatrixAddition.java
+++ lab program/MatrixAddition.java
@@ -1,54 +1,41 @@
-import java.util.Scanner;
+import java.util.*;
 
-
+/*  Develop a JAVA program to add TWO matrices of suitable order N (The value of N should be read from
+command line arguments). */
 public class MatrixAddition {
     public static void main(String[] args) {
-        // Check if N is provided through command line
-        if (args.length != 1) {
-            System.out.println("Usage: java MatrixAddition <N>");
-            return;
-        }
-
-
-        int N = Integer.parseInt(args[0]);
+        Scanner sc = new Scanner(System.in);
+        
+        System.out.print("Enter the size of the matrix (N): ");
+        int N = sc.nextInt();
+        
         int[][] A = new int[N][N];
         int[][] B = new int[N][N];
         int[][] C = new int[N][N];
 
-
-        Scanner sc = new Scanner(System.in);
-
-
-        // Read first matrix
-        System.out.println("Enter elements of Matrix A (" + N + "x" + N + "):");
-        for (int i = 0; i < N; i++) {
-            for (int j = 0; j < N; j++) {
+        // Initialize matrices with some values
+        System.out.println("Enter the elements of Matrix A ("+N+"x"+N+"):");
+        for(int i=0; i<N; i++){
+            for(int j=0; j<N; j++){
                 A[i][j] = sc.nextInt();
             }
         }
-
-
-        // Read second matrix
-        System.out.println("Enter elements of Matrix B (" + N + "x" + N + "):");
-        for (int i = 0; i < N; i++) {
-            for (int j = 0; j < N; j++) {
+        System.out.println("Enter the elements of Matrix B ("+N+"x"+N+"):");
+        for(int i=0; i<N; i++){
+            for(int j=0; j<N; j++){
                 B[i][j] = sc.nextInt();
             }
         }
-
-
-        // Add the two matrices
-        for (int i = 0; i < N; i++) {
-            for (int j = 0; j < N; j++) {
+        // Add the matrices
+        for(int i=0; i<N; i++){
+            for(int j=0; j<N; j++){
                 C[i][j] = A[i][j] + B[i][j];
             }
         }
-
-
-        // Display the result
+        // Print the result
         System.out.println("\nResultant Matrix (A + B):");
-        for (int i = 0; i < N; i++) {
-            for (int j = 0; j < N; j++) {
+        for(int i=0; i<N; i++){
+            for(int j=0; j<N; j++){
                 System.out.print(C[i][j] + "\t");
             }
             System.out.println();
