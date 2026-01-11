c008a4da6aade6409f3fd41e3ff56abcb66d5e96
Rahul G <rahulg23102006@gmail.com>
2026-01-11 12:53:14 +0530

    ......

--- lab program/MatrixAddition.java
+++ lab program/MatrixAddition.java
@@ -1,19 +1,12 @@
 import java.util.*;
-
-/*  Develop a JAVA program to add TWO matrices of suitable order N (The value of N should be read from
-command line arguments). */
 public class MatrixAddition {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        
         System.out.print("Enter the size of the matrix (N): ");
         int N = sc.nextInt();
-        
         int[][] A = new int[N][N];
         int[][] B = new int[N][N];
         int[][] C = new int[N][N];
-
-        // Initialize matrices with some values
         System.out.println("Enter the elements of Matrix A ("+N+"x"+N+"):");
         for(int i=0; i<N; i++){
             for(int j=0; j<N; j++){
@@ -26,13 +19,11 @@ public class MatrixAddition {
                 B[i][j] = sc.nextInt();
             }
         }
-        // Add the matrices
         for(int i=0; i<N; i++){
             for(int j=0; j<N; j++){
                 C[i][j] = A[i][j] + B[i][j];
             }
         }
-        // Print the result
         System.out.println("\nResultant Matrix (A + B):");
         for(int i=0; i<N; i++){
             for(int j=0; j<N; j++){
